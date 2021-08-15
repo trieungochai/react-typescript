f10e423936c3291425b748da50fd610f24175433
Cai <trieungochai.dev@gmail.com>
2021-08-15 22:50:51 +0700

    State with Typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import ReactDOM from "react-dom";
 
+import GuestList from "./state/GuestList";
+
 const App = () => {
   return (
     <div>
-      <h1>See you Space Cowboy!!!</h1>
+      <GuestList />
     </div>
   );
 };
