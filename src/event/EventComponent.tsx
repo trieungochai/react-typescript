76f1ed4376b5896fb86d214035aec98aa2e6e80a
Cai <trieungochai.dev@gmail.com>
2021-08-16 15:34:16 +0700

    Handling Drag Events

--- src/event/EventComponent.tsx
+++ src/event/EventComponent.tsx
@@ -1,11 +1,20 @@
+import React from "react";
+
 const EventComponent: React.FC = () => {
-  const onChange = (event: React.ChangeEvent<HTMLInputElement> | undefined) => {
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event);
+  };
+
+  const onDragStart = (event: React.DragEvent<HTMLDivElement>) => {
     console.log(event);
   };
 
   return (
     <div>
       <input onChange={onChange} />
+      <div draggable onDragStart={onDragStart}>
+        Drag me!
+      </div>
     </div>
   );
 };
