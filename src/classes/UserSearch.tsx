0b0845e32284d72397fb6b737f61abaae88be2b2
Cai <trieungochai.dev@gmail.com>
2021-08-16 16:26:38 +0700

    Typescript with Class Components

---
+++ src/classes/UserSearch.tsx
@@ -0,0 +1,52 @@
+import { Component } from "react";
+
+interface User {
+  name: string;
+  age: number;
+}
+
+interface UserSearchProps {
+  users: User[];
+}
+
+interface UserSearchState {
+  name: string;
+  user: User | undefined;
+}
+
+class UserSearch extends Component<UserSearchProps> {
+  state: UserSearchState = {
+    name: "",
+    user: undefined,
+  };
+
+  onClick = () => {
+    const foundUser = this.props.users.find((user) => {
+      return user.name === this.state.name;
+    });
+
+    this.setState({ user: foundUser });
+  };
+
+  render() {
+    const { name, user } = this.state;
+
+    return (
+      <div>
+        <h3>Find User</h3>
+        <input
+          value={name}
+          onChange={(e) => this.setState({ name: e.target.value })}
+        />
+        <button onClick={this.onClick}>Find User</button>
+        <div>
+          <h3>User Details</h3>
+          <p>Name: {user && user.name}</p>
+          <p>Age: {user && user.age}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default UserSearch;
